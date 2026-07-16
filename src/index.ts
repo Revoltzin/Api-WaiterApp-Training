fa8412ff368fcdc8dca7906696eaa0afacfbd9ee
Revoltzin <jat1963@gmail.com>
2026-07-16 19:07:20 -0300

    refactoring: middlware organization fixed

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import { errorHandler }  from "./middlewares/errorHandler"
 
 const app = express()
+app.use(express.json());
 
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
 });
 
-app.use(express.json());
 app.use(errorHandler)
 
 const PORT = process.env.PORT ?? 3000;
